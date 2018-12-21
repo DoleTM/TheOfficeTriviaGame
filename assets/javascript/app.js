5a3d57b377bb12cacb734c6e89212817bbe45ec6
DoleTM <adylantoremember@gmail.com>
2018-12-20 23:58:40 -0500

    still has issues, need to resumbit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,12 +24,12 @@ var triviaQuestions = [{
     selection: ["Pretzel Day", "Michael's Last Day", "Diversity Day", "Recyclops Day"],
     answer: "Pretzel Day"
 }];
-
+// Loops through the array of questions
 for (let i = 0; i < triviaQuestions.length; i++) {
     var questionDiv = $("#questions-div");
     // This adds the questions to display on the html
     questionDiv.append("<div>" + triviaQuestions[i].question + "</div>");
-// This adds the selection to each question as a radio button to the html
+    // This adds the selection to each question as a radio button to the html
     $("#questions-div").append('<label class="container">' + '<input type="radio" name="radio">' + '<span class="checkmark">' + '</span>' + triviaQuestions[i].selection[0] + '</label>');
     $("#questions-div").append('<label class="container">' + '<input type="radio" name="radio">' + '<span class="checkmark">' + '</span>' + triviaQuestions[i].selection[1] + '</label>');
     $("#questions-div").append('<label class="container">' + '<input type="radio" name="radio">' + '<span class="checkmark">' + '</span>' + triviaQuestions[i].selection[2] + '</label>');
@@ -37,58 +37,62 @@ for (let i = 0; i < triviaQuestions.length; i++) {
 }
 
 // Variable for the timer
-var timerDisplay = 120;
+var timerCurrent = 12000;
 
-function timerStart() {
-    $("#timer").text("Time Remaining: " + timerDisplay);
-    setInterval(timerDisplay, 1000)
-    timerDisplay--;
-    if (timerDisplay === 0) {
+// When the timer stops...
+function timerStop() {
+    clearInterval();
+    displayResults();
+};
+
+// This will countdown the seconds until 0 is reached
+function displayCurrentTime() {
+    timerCurrent--;
+    $("#timer").append("Time remaining: " + timerCurrent);
+    if (timerCurrent === 0) {
         timerStop();
         $("#timer").empty();
     }
-}
+};
 
-function timerStop() {
-    clearInterval();
-    resultDisplay();
-}
-// By clicking the start button it will "start" the game.
-$("#start").click(function() {
-    // Hide the start page.
-    $(".start-div").hide();
-    // Make the trivia div visible.
-    $("#trivia-div").show();
+// This will start the timer
+function timerStart() {
+    $("#timer").append("Time remaining: " + timerCurrent);
+    //setInterval(displayCurrentTime(1000));
+    displayResults();
+};
 
-    // Show the time remaining
-    timerStart();
-    if (timerDisplay === 0) {
-        timerStop();
-        $("#trivia-div").hide();
-        $("#result-div").show();
-        resultDisplay();
-    } else if ($("#finish").click() === true) {
-        $("#trivia-div").hide();
-        $("#result-div").show();
-        resultDisplay();
-    }
-});
 // Varibles for the results page
-    var userInput;
-    var userCorrect;
-    var displayCorrect = $("#right");
-    var displayWrong = $("#wrong");
-    var displayNoInput = $("#undefined");
+var userInput;
+var userCorrect;
+var displayCorrect = $("#right");
+var displayWrong = $("#wrong");
+var displayNoInput = $("#undefined");
 
-function resultDisplay() {
+// This function will hide and clear trivia data,
+// loop though the users answers and display the score
+function displayResults() {
+    $("#trivia-div").hide().empty();
+    $("#result-div").show();
     for (let i = 0; i < triviaQuestions.length; i++)
-        userInput = $().();
-        userCorrect = triviaQuestions[i].length
-    if (userInput === answer) {
+        userCorrect = triviaQuestions[i].answer.length;
+        userInput = $("<input type='radio' name='radio' checked='checked'>")
+    if (userInput === triviaQuestions.answer) {
         displayCorrect++;
-    } else if (userInput !== answer) {
+    } else if (userInput !== triviaQuestions.answer) {
         displayWrong++;
     } else {
         displayNoInput++;
     }
 }
+
+// By clicking the start button it will "start" the game.
+$("#start").click(function() {
+    $(".start-div").hide();
+    $("#trivia-div").show();
+});
+
+// By clicking the finish button it will display the results page
+$("#finish").click(function() {
+    displayResults();
+});
